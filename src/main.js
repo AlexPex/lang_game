0e541444c0a232c91d76350431908666aff72ff0
Alexandr <alexprogrammer944@gmail.com>
2024-03-13 11:24:15 +0200

    install packages

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import "bootstrap/dist/css/bootstrap.css"
+
+// Import our custom CSS
+// import '../scss/styles.scss'
+// // Import all of Bootstrap's JS
+// import "bootstrap/dist/js/bootstrap.js";
+
+createApp(App).mount('#app')
